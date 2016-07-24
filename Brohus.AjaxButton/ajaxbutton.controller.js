fcf8bfc5d21706db6f7b8d28192da2ce82b01940
Anders Brohus <andez@live.dk>
2016-07-24 19:27:52 +0200

    Version 1.0

---
+++ Brohus.AjaxButton/ajaxbutton.controller.js
@@ -0,0 +1,15 @@
+﻿angular.module("umbraco").controller("Brohus.AjaxButtonController", function ($scope, $http) {
+	$scope.loading = false;
+	$scope.buttonText = $scope.model.config.buttonText;
+
+	$scope.cyCallUrl = function () {
+		$scope.loading = true;
+		$http.get($scope.model.config.ajaxUrl).then(function (response) {
+			$scope.return = "The URL was called successfully."
+			$scope.loading = false;
+		}, function (response) {
+			$scope.return = "An error occurred."; 
+			$scope.loading = false;
+		});
+	};
+});
